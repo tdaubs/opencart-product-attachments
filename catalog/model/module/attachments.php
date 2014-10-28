02a47167404a24646d5d9bb39a73e0bf39946447
david peach <dpeach27@gmail.com>
2014-10-28 13:56:21 +0000

    Update attachments.php

--- catalog/model/module/attachments.php
+++ catalog/model/module/attachments.php
@@ -5,12 +5,12 @@ class ModelModuleAttachments extends Model {
 		$return_array	=	array();
 
 		$this->load->model('setting/setting');
-		$pdf_setting	=	$this->model_setting_setting->getSetting('product_pdfs');
-		if( !empty($pdf_setting) && $pdf_setting['product_pdfs_status'] == '1' ){
+		$pdf_setting	=	$this->model_setting_setting->getSetting('attachments');
+		if( !empty($pdf_setting) && $pdf_setting['attachments_status'] == '1' ){
 			$result	=	$this->db->query("SELECT * FROM " . DB_PREFIX . $table . "_to_attachment WHERE " . $table . "_id = '$id' ");
 			if($result->num_rows){
 				foreach ($result->rows as $row) {
-					$q 	=	$this->db->query("SELECT * FROM " . DB_PREFIX . "product_pdfs WHERE attachment_id = ".$row['attachment_id'] );
+					$q 	=	$this->db->query("SELECT * FROM " . DB_PREFIX . "attachments WHERE attachment_id = ".$row['attachment_id'] );
 					
 					$filename		=	$q->row['filename'];
 					$display_name	=	$q->row['display_name'];
