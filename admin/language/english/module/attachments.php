707218d0abc2efcae7b1c03ae50baa1cbd22c306
david peach <dpeach27@gmail.com>
2014-10-28 14:38:44 +0000

    Update attachments.php

--- admin/language/english/module/attachments.php
+++ admin/language/english/module/attachments.php
@@ -5,6 +5,7 @@ $_['entry_pdf_displayname']			=	'Display Name';
 $_['entry_pdf_filename']			=	'File Name';
 $_['entry_num_attached']			=	'N<sup>o</sup> of products attached to';
 $_['entry_products_attached_to']	=	'Products attached to';
+$_['entry_categories_attached_to']	=	'Categories attached to';
 $_['entry_delete_pdf']				=	'Delete Attachment';
 
 $_['button_add_pdf']				=	'Upload Attachment';
