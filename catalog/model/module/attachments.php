1e5bedec3e389e8e5850a527cd936b9f51ca5a32
david peach <dpeach27@gmail.com>
2014-10-28 13:54:45 +0000

    Update and rename product_pdfs.php to attachments.php

--- catalog/model/module/attachments.php
+++ catalog/model/module/attachments.php
@@ -1,25 +1,25 @@
 <?php
-class ModelModuleProductPdfs extends Model {
+class ModelModuleAttachments extends Model {
 	
-	public function getGimmepdfs($product_id) {
+	public function get_the_attachments($id, $table="product") {
 		$return_array	=	array();
 
 		$this->load->model('setting/setting');
 		$pdf_setting	=	$this->model_setting_setting->getSetting('product_pdfs');
 		if( !empty($pdf_setting) && $pdf_setting['product_pdfs_status'] == '1' ){
-			$result	=	$this->db->query("SELECT * FROM ".DB_PREFIX."product_to_pdf WHERE product_id = '$product_id' ");
+			$result	=	$this->db->query("SELECT * FROM " . DB_PREFIX . $table . "_to_attachment WHERE " . $table . "_id = '$id' ");
 			if($result->num_rows){
 				foreach ($result->rows as $row) {
-					$q 	=	$this->db->query("SELECT * FROM ".DB_PREFIX."product_pdfs WHERE pdf_id = ".$row['pdf_id'] );
+					$q 	=	$this->db->query("SELECT * FROM " . DB_PREFIX . "product_pdfs WHERE attachment_id = ".$row['attachment_id'] );
 					
 					$filename		=	$q->row['filename'];
 					$display_name	=	$q->row['display_name'];
-					$pdf_id			=	$q->row['pdf_id'];
+					$attachment_id	=	$q->row['attachment_id'];
 
 					$return_array[]	=	array(
 						'filename'		=>	$filename,
 						'display_name'	=>	$display_name,
-						'pdf_id'		=>	$pdf_id
+						'attachment_id'	=>	$attachment_id
 						);
 				}
 			}
